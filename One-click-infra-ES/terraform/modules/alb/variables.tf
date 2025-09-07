ed72a04e94296ae99b05808a916fc907d9bd66fc
ThakurDhani <thakurdhani143@gmail.com>
2025-09-07 07:34:45 +0530

    Update variables.tf

--- One-click-infra-ES/terraform/modules/alb/variables.tf
+++ One-click-infra-ES/terraform/modules/alb/variables.tf
@@ -1,6 +1,5 @@
 variable "public_subnets" {}
 variable "vpc_id" {}
-variable "sg_id" {}               # ALB Security Group
-variable "target_group_port" {
-  default = 9200                 # Elasticsearch listens on port 9200
-}
+variable "sg_id" {}
+
+# no need for target_group_port anymore (we define per TG)
