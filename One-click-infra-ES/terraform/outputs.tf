60c7fa9f968edf0c897be1f27d17680b4babfc62
ThakurDhani <thakurdhani143@gmail.com>
2025-09-07 08:24:22 +0530

    Update outputs.tf

--- One-click-infra-ES/terraform/outputs.tf
+++ One-click-infra-ES/terraform/outputs.tf
@@ -1,3 +1,27 @@
-output "bastion_public_ip" {
-  value = module.bastion.public_ip
+##############################
+# Root Outputs
+##############################
+
+# ALB DNS - Use this to access Kibana/Elasticsearch
+output "alb_dns" {
+  description = "DNS name of the Application Load Balancer"
+  value       = module.alb.alb_dns
+}
+
+# Elasticsearch Target Group ARN (optional if needed elsewhere)
+output "es_target_group_arn" {
+  description = "ARN of the Elasticsearch Target Group"
+  value       = module.alb.es_target_group_arn
+}
+
+# Kibana Target Group ARN (optional if needed elsewhere)
+output "kibana_target_group_arn" {
+  description = "ARN of the Kibana Target Group"
+  value       = module.alb.kibana_target_group_arn
+}
+
+# Auto Scaling Group Name - used by Ansible dynamic inventory (with tags)
+output "asg_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = module.asg.asg_name
 }
