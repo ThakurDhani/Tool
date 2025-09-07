603756b3d5d7ad205ab1a95ffcb3792b84dd2c9a
ThakurDhani <thakurdhani143@gmail.com>
2025-09-07 10:09:49 +0530

    Update outputs.tf

--- One-click-infra-ES/terraform/outputs.tf
+++ One-click-infra-ES/terraform/outputs.tf
@@ -2,32 +2,56 @@
 # Root Outputs
 ##############################
 
-# ALB DNS - Use this to access Kibana/Elasticsearch
+# ----------------------------
+# ALB + Endpoints
+# ----------------------------
+
+# ALB DNS - Base DNS name of the Application Load Balancer
 output "alb_dns" {
   description = "DNS name of the Application Load Balancer"
   value       = module.alb.alb_dns
 }
 
-# Elasticsearch Target Group ARN (optional if needed elsewhere)
+# Friendly Kibana URL
+output "kibana_url" {
+  description = "Kibana dashboard URL"
+  value       = "http://${module.alb.alb_dns}"
+}
+
+# Friendly Elasticsearch URL
+output "elasticsearch_url" {
+  description = "Elasticsearch endpoint URL"
+  value       = "http://${module.alb.alb_dns}/elasticsearch"
+}
+
+# ----------------------------
+# Target Groups (infra only, optional)
+# ----------------------------
+
 output "es_target_group_arn" {
   description = "ARN of the Elasticsearch Target Group"
   value       = module.alb.es_target_group_arn
 }
 
-# Kibana Target Group ARN (optional if needed elsewhere)
 output "kibana_target_group_arn" {
   description = "ARN of the Kibana Target Group"
   value       = module.alb.kibana_target_group_arn
 }
 
-# Auto Scaling Group Name - used by Ansible dynamic inventory (with tags)
+# ----------------------------
+# Auto Scaling Group
+# ----------------------------
+
 output "asg_name" {
-  description = "Name of the Auto Scaling Group"
+  description = "Name of the Auto Scaling Group (used by Ansible dynamic inventory)"
   value       = module.asg.asg_name
 }
 
-# Bastion Public IP - used by Jenkins to copy SSH key
+# ----------------------------
+# Bastion Host
+# ----------------------------
+
 output "bastion_public_ip" {
-  description = "Public IP address of the Bastion host"
+  description = "Public IP address of the Bastion host (used by Jenkins to copy SSH key)"
   value       = module.bastion.public_ip
 }
