379531f7c584b29b9700ab0f2ffd8962128123aa
ThakurDhani <thakurdhani143@gmail.com>
2025-09-09 22:18:55 +0530

    Update outputs.tf

--- One-click-infra-ES/terraform/modules/alb/outputs.tf
+++ One-click-infra-ES/terraform/modules/alb/outputs.tf
@@ -2,10 +2,6 @@ output "alb_dns" {
   value = aws_lb.alb.dns_name
 }
 
-output "es_target_group_arn" {
-  value = aws_lb_target_group.es_tg.arn
-}
-
-output "kibana_target_group_arn" {
-  value = aws_lb_target_group.kibana_tg.arn
+output "target_group_arn" {
+  value = aws_lb_target_group.tg.arn
 }
