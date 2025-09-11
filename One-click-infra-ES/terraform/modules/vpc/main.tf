b50fb524b79ba040a84b2ab30c47d0ecf771a3ab
ThakurDhani <thakurdhani143@gmail.com>
2025-09-11 18:02:41 +0530

    Update main.tf

--- One-click-infra-ES/terraform/modules/vpc/main.tf
+++ One-click-infra-ES/terraform/modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = { Name = "main-vpc" }
+  tags = { Name = "ES-vpc" }
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -62,3 +62,4 @@ resource "aws_route_table_association" "private" {
   subnet_id = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
+
